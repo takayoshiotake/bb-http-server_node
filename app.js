e0344e09faa3093240178398a6be16a2e08acb15
OTAKE Takayoshi <takayoshiotake@users.noreply.github.com>
2016-10-03 23:39:53 +0900

    Change file structure

--- app.js
+++ app.js
@@ -1,97 +1,11 @@
-const http = require('http')
-const url = require('url')
-const path = require("path")
-const fs = require('fs')
+'use strict';
 
-const PORT = 8080
-const ADDRESS = '0.0.0.0'
+var bbhttpserver = require('./')
 
-function default_content_for_status(status) {
-  try {
-    const data = fs.readFileSync(`${__dirname}/assets/${status}.html`)
-    return {'status': status, 'header': {'Content-Type': 'text/html'}, 'data': data}
-  }
-  catch (err) {
-    return {'status': status, 'header': {'Content-Type': 'text/plain'}, 'data': `${status} ${http.STATUS_CODES[status]}`}
-  }
-}
+const port = 8080
+const address = '127.0.0.1'
 
-http.createServer((req, res) => {
-  const parsed_url = url.parse(req.url)
-  var filepath = path.join(__dirname + '/public', parsed_url.pathname)
-  try {
-    if (fs.statSync(filepath).isDirectory()) {
-      if (parsed_url.pathname.slice(-1) != '/') {
-        if (parsed_url.query) {
-          res.writeHead(302, {'Location': parsed_url.pathname + '/?' + parsed_url.query})
-        }
-        else {
-          res.writeHead(302, {'Location': parsed_url.pathname + '/'})
-        }
-        res.end()
-        return
-      }
-
-      var provisional = path.join(filepath, 'index.cgi.js')
-      try {
-        fs.accessSync(provisional, fs.constants.F_OK)
-      }
-      catch (err) {
-        provisional = path.join(filepath, 'index.html')
-      }
-      filepath = provisional
-    }
-    else {
-      fs.accessSync(filepath, fs.constants.F_OK)
-    }
-  }
-  catch (err) {
-    const content = default_content_for_status(404)
-    res.writeHead(content.status, content.header)
-    res.end(content.data)
-    return
-  }
-
-  fs.readFile(filepath, 'binary', (err, data) => {
-    if (err) {
-      console.log(err)
-      const content = default_content_for_status(500)
-      res.writeHead(content.status, content.header)
-      res.end(content.data)
-      return
-    }
-    if (filepath.endsWith('.cgi.js')) {
-      let args = {
-        'script': filepath,
-        'method': req.method,
-        'url': parsed_url,
-      } // bridge values to script
-      try {
-        // HACK: eval(data) returns a function; call function with args.
-        var content = eval(data)(args)
-        if (content.data) {
-          res.writeHead(content.status, content.header)
-          res.end(content.data, 'binary')
-        }
-        else {
-          content = default_content_for_status(content.status)
-          res.writeHead(content.status, content.header)
-          res.end(content.data)
-        }
-      }
-      catch (err) {
-        console.log(err)
-        const content = default_content_for_status(500)
-        res.writeHead(content.status, content.header)
-        res.end(content.data)
-      }
-    }
-    else {
-      res.writeHead(200, {})
-      res.end(data, 'binary')
-    }
-  })
-}).listen(PORT, ADDRESS)
+bbhttpserver.run(__dirname, port, address)
 
 console.log('Available on:')
-console.log('  http://127.0.0.1:8080/')
+console.log(`  http://${address}:${port}/`)
---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * bb-http-server
+ */
+
+'use strict';
+
+module.exports = require('./lib/bbhttpserver');
